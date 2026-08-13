020bf28925e5a63da060a3695a9d317000d30d5a
zoharb0202 <zoharb0202@gmail.com>
2026-08-13 12:25:09 +0300

    Create InvalidRequestException.java

---
+++ engine/src/guessmarket/engine/exception/InvalidRequestException.java
@@ -0,0 +1,11 @@
+package guessmarket.engine.exception;
+
+public class InvalidRequestException extends RuntimeException
+{
+    private static final long serialVersionUID = 1L;
+
+    public InvalidRequestException(String message)
+    {
+        super(message);
+    }
+}
